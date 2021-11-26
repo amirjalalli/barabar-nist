415ec1f6b1267e237b070d9840421f2acc58722f
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2021-11-26 17:12:45 +0330

    barabar-nist

---
+++ js.js
@@ -0,0 +1,10 @@
+var num1 = 12;
+var num2 = 4;
+var num3 = 3;
+if (num2 * num3 === 10){
+    alert("your number is barabar")
+} else if (num2 + num3 === 5){
+    alert("your number is plus plus")
+} else {
+    alert(" dada barabar nist")
+}
